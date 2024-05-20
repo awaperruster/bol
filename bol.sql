21d6a2e32b12a31e8a5fd98d06db11d83131f1d2
slxtt <kripsday@gmail.com>
2024-05-21 03:15:25 +0600

    db 2.0

--- bol.sql
+++ bol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Май 21 2024 г., 00:13
+-- Время создания: Май 21 2024 г., 00:15
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -43,6 +43,52 @@ CREATE TABLE `doctors` (
 INSERT INTO `doctors` (`id`, `name`, `phone`, `email`, `img`, `id_specialties`) VALUES
 (1, 'Абдуаля Ибрагим Ишак - Санин ', '+7999999999', '123@123', '1.jpg', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `specialties`
+--
+
+CREATE TABLE `specialties` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `specialties`
+--
+
+INSERT INTO `specialties` (`id`, `name`, `img`) VALUES
+(1, 'Педиатрия', ''),
+(2, 'Кардиология', ''),
+(3, 'Ортопедия', ''),
+(4, 'ГинеКАЛогия', ''),
+(5, 'Неврология', ''),
+(6, 'Психиатрия', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `phone`, `address`, `email`, `password`) VALUES
+(1, '123', '12312', '123', '123@1', '202cb962ac59075b964b07152d234b70');
+
 --
 -- Индексы сохранённых таблиц
 --
@@ -55,6 +101,18 @@ ALTER TABLE `doctors`
   ADD KEY `id_specialties` (`id_specialties`);
 
 --
+-- Индексы таблицы `specialties`
+--
+ALTER TABLE `specialties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -62,6 +120,18 @@ ALTER TABLE `doctors`
 -- AUTO_INCREMENT для таблицы `doctors`
 --
 ALTER TABLE `doctors`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `specialties`
+--
+ALTER TABLE `specialties`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
